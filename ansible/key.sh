d1c606bdc06d62926c8a4582157ea4e75331e35e
philippe <tchevass@gmail.com>
2015-09-03 16:30:12 -0400

    Added bash script to remove the fingerprint so that we can use our ansible scripts

---
+++ ansible/key.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script allow us to ssh into the machine and do our ansible command.
+# Execute this script if you just did :
+# 	vagrant destroy and vagrant up
+
+ansible -m ping all
+
+for (( i=0; i<=2; i++)) 
+do
+   ssh-keygen -f "$HOME/.ssh/known_hosts" -R 192.168.35.1$i
+done
